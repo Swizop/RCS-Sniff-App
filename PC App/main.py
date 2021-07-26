4f971012e52e53a77baeb069cbbd12450b0c8f26
Matei <matei.neagu19@yahoo.com>
2021-07-26 15:52:12 +0300

    add new case

--- PC App/main.py
+++ PC App/main.py
@@ -4,6 +4,10 @@ def main():
     capture = pyshark.FileCapture('20210726-124925.cap')
     g = open("output.txt", 'w')
     eventsNr = 0
+
+    oneSentList = [0]                        #containers and variables used in the case of phone 1 sending a text message
+    oneSentCharList = [0]
+    oneSentUnresolved = 0
     
     for pkt in capture:
         try:
@@ -16,7 +20,39 @@ def main():
                     and pkt.ip.len == '848' and pkt.tcp.flags_push == '1':
                 eventsNr += 1
                 g.write(f"Event {eventsNr}. Phone 2 is writing a message for Phone 1\n")
+            
 
+            elif pkt.ip.src == '10.0.0.1' and pkt.ip.dst in ['216.239.36.128', '216.239.36.129','216.239.36.130'] \
+                and int(pkt.ip.len) >= 250 and pkt.tcp.flags_push == '1':
+                oneSentCharList.append(int(pkt.ip.len) - 250)
+                oneSentUnresolved += 1
+                oneSentList.append("2ACK")
+            
+            elif oneSentUnresolved > 0 and pkt.ip.dst == '10.0.0.1' and pkt.ip.src in ['216.239.36.128', '216.239.36.129','216.239.36.130']:
+                if pkt.tcp.flags_push == '1':
+                    try:
+                        i = oneSentList.index("2PUSH")
+                        oneSentList[i] = "1ACK"
+                    except ValueError:
+                        pass
+                elif pkt.tcp.flags_ack == '1':
+                    try:
+                        i = oneSentList.index("2ACK")
+                        oneSentList[i] = "2PUSH"
+                    except ValueError:
+                        pass
+
+            elif oneSentUnresolved > 0 and pkt.ip.src == '10.0.0.1' and pkt.ip.dst in ['216.239.36.128', '216.239.36.129','216.239.36.130']:
+                if pkt.tcp.flags_ack == '1':
+                    try:
+                        i = oneSentList.index("1ACK")
+                        oneSentList[i] = "DONE"
+                        oneSentUnresolved -= 1
+                        eventsNr += 1
+                        g.write(f"Event {eventsNr}. Phone 1 sent a text message to Phone 2, which is {oneSentCharList[i]} characters long\n")
+
+                    except ValueError:
+                        pass
         except AttributeError:
             continue
 
